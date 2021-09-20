1b1079169c5290523c5d519250e4be5224625cd7
Ederson Ferreira <edersondeveloper@gmail.com>
2021-09-20 14:34:52 -0400

    adicionando funções para alterar e deletar tarefa

--- src/main/kotlin/kotlinautas/controllers/TarefaController.kt
+++ src/main/kotlin/kotlinautas/controllers/TarefaController.kt
@@ -15,6 +15,8 @@ import org.valiktor.ConstraintViolationException
 fun Route.tarefaRoute() {
     route("/tarefas") {
         insereTarefa()
+        atualizaTarefa()
+        apagaTarefa()
     }
 }
 
@@ -43,6 +45,53 @@ fun Route.insereTarefa() {
         } catch (erro: Exception) {
             println(erro)
             return@post call.respondText("Erro ao criar tarefa", status = HttpStatusCode.InternalServerError)
+        }
+    }
+}
+fun Route.atualizaTarefa() {
+    put("{id}") {
+        try {
+            val id = call.parameters["id"] ?: return@put call.respondText("Informe um ID", status = HttpStatusCode.PaymentRequired)
+
+            val tarefa = call.receive<Tarefa>()
+
+            val edicao = transaction {
+                Tarefas.update({ Tarefas.id eq id }) {
+                    it[nome] = tarefa.nome
+                    it[estado] = tarefa.estado
+                }
+            }
+
+            if (edicao.equals(0)) {
+                return@put call.respondText("Erro ao modificar tarefa", status = HttpStatusCode.InternalServerError)
+            }
+
+            return@put call.respond(Tarefa(id, tarefa.nome, tarefa.estado, tarefa.usuarioId))
+        }catch (ex: ConstraintViolationException){
+            println(returnLocatedValidationErros(ex))
+            return@put call.respondText("Erro ao atualizar tarefa", status = HttpStatusCode.InternalServerError)
+        } catch (erro: Exception) {
+            return@put call.respondText("Erro ao atualizar tarefa", status = HttpStatusCode.InternalServerError)
+        }
+    }
+}
+
+fun Route.apagaTarefa() {
+    delete("{id}") {
+        try {
+            val id = call.parameters["id"] ?: return@delete call.respondText("Informe um ID", status = HttpStatusCode.PaymentRequired)
+
+            val remocao = transaction {
+                Tarefas.deleteWhere { Tarefas.id eq id }
+            }
+
+            if (remocao.equals(0)) {
+                return@delete call.respondText("Tarefa não encontrado", status = HttpStatusCode.InternalServerError)
+            }
+
+            return@delete call.respondText("Tarefa apagada")
+        } catch (erro: Exception) {
+            return@delete call.respondText("Erro ao apagar tarefa", status = HttpStatusCode.InternalServerError)
         }
     }
 }
